5f588d7d1627caabbfaa9f534d17d9efe5baaaad
Oleg Sobolev <iiiyxep@list.ru>
2020-06-27 15:09:01 +0300

    socket.io -> websockets

--- app.js
+++ app.js
@@ -3,52 +3,12 @@ const express = require('express')
 //declare the variable that is going to be linked to express
 const app = express()
 //variable for feature media
-let http = require('http').Server(app)
+let http = require('http').Server(app);
 //where the app will be running
-const port = process.env.PORT || 3000
-//require socket.io library
-let io = require('socket.io')(http)
+const port = process.env.PORT || 3001;
 //configure static hosting for the public folder
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-http.listen(port, () =>
-{
-    console.log('Listening on', port)
-})
-
-io.on('connection', socket => {
-    console.log('New user is connected')
-    //create a room if doesn't exist or join to it
-    socket.on('create or join', room => {
-        console.log('create or join to room', room)
-        //How many users the room has
-        const myRoom = io.sockets.adapter.rooms[room] || {length: 0}
-        const numClients = myRoom.length
-        console.log('Room:', room, 'has', numClients+1, 'clients')
-        //If the room doesn't exist we create it
-        if (numClients == 0)
-        {
-            socket.join(room)
-            socket.emit('created', room)
-        } else if(numClients == 1)
-        {
-            socket.join(room)
-            socket.emit('joined', room)
-        } else {
-            socket.emit('full', room)
-        }
-    })
-    socket.on('ready', room => {
-        socket.broadcast.to(room).emit('ready')
-    })
-    socket.on('candidate', event => {
-        socket.broadcast.to(event.room).emit('candidate', event)
-    })
-    socket.on('offer', event => {
-        socket.broadcast.to(event.room).emit('offer', event.sdp)
-    })
-    socket.on('answer', event => {
-        socket.broadcast.to(event.room).emit('answer', event.sdp)
-    })    
-})
-
+http.listen(port, () => {
+  console.log('Listening on', port)
+});
--- public/main.js
+++ public/main.js
@@ -1,157 +1,183 @@
-let divSelectRoom = document.getElementById('selectRoom')
-let inputRoomName = document.getElementById('roomName')
-let btnGoRoom = document.getElementById('goRoom')
-let divConsultingRoom = document.getElementById('consultingRoom')
-let localVideo = document.getElementById('localVideo')
-let remoteVideo = document.getElementById('remoteVideo')
+let divSelectRoom = document.getElementById('selectRoom');
+let inputRoomName = document.getElementById('roomName');
+let btnGoRoom = document.getElementById('goRoom');
+let divConsultingRoom = document.getElementById('consultingRoom');
+let localVideo = document.getElementById('localVideo');
+let remoteVideo = document.getElementById('remoteVideo');
 
 //variables for feature videos
-let roomName, localStream, remoteStream, rtcPeerConnection, isCaller
+let roomName, localStream, remoteStream, rtcPeerConnection, isCaller;
 
 //define the stun servers
-const iceServers = {
-    'iceServer': [
-        {'urls': 'stun:stun.services.mozilla.com'},
-        {'urls': 'stun:stun.l.google.com:19302'}
-    ]
-}
+const rtcConfig = {
+  iceServers: [
+    {'urls': 'stun:stun.services.mozilla.com'},
+    {'urls': 'stun:stun.l.google.com:19302'}
+  ]
+};
 
 const streamConstraints = {
-    audio: true,
-    video: true
-}
+  audio: true,
+  video: true
+};
 
-const socket = io()
+const ws = new WebSocket('ws://localhost:5442/ws-test');
+
+const sendMessage = (command, data) => {
+  ws.send(JSON.stringify({command, data}));
+};
 
 btnGoRoom.onclick = () => {
-    if(inputRoomName.value === '')
-    {
-        alert ("Please enter a room name")
-    } else {
-        roomName = inputRoomName.value
-        socket.emit('create or join', roomName)
-        divSelectRoom.style = "display: none"
-        divConsultingRoom.style = "display: block"
-    }
-}
+  if (inputRoomName.value === '') {
+    alert('Please enter a room name')
+  } else {
+    roomName = inputRoomName.value;
+    sendMessage('create or join', {room: roomName});
+    divSelectRoom.style = 'display: none';
+    divConsultingRoom.style = 'display: block'
+  }
+};
 
-socket.on('created', room => 
-{
-    navigator.mediaDevices.getUserMedia(streamConstraints)
-        .then(stream => {
-            localStream = stream
-            localVideo.srcObject = stream
-            isCaller = true
-        })
-        .catch(err => {
-            console.log('An error ocurred', err)
-        })
-})
+const withRoomCheck = (cb) => (room, ...rest) => {
+  if (room !== roomName) {
+    console.warn('wrong room name', room);
+    return;
+  }
 
-socket.on('joined', room => 
-{
-    navigator.mediaDevices.getUserMedia(streamConstraints)
-        .then(stream => {
-            localStream = stream
-            localVideo.srcObject = stream      
-            socket.emit('ready', roomName)      
-        })
-        .catch(err => {
-            console.log('An error ocurred', err)
-        })
-})
+  cb(...rest);
+};
 
-socket.on('full', room =>
-{
-    alert('The following session is full, please create another session or wait')
-})
+const handleCreated = withRoomCheck(() => {
+  navigator.mediaDevices.getUserMedia(streamConstraints)
+      .then(stream => {
+        localStream = stream;
+        localVideo.srcObject = stream;
+        isCaller = true;
+      })
+      .catch(err => {
+        console.log('An error occurred', err)
+      })
+});
 
-socket.on('ready',  () =>
-{
-    if(isCaller)
-    {
-        rtcPeerConnection = new RTCPeerConnection(iceServers)
-        rtcPeerConnection.onicecandidate = onIceCandidate
-        rtcPeerConnection.ontrack = onAddStream
-        rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream)
-        rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream)
-        rtcPeerConnection.createOffer()
-            .then(sessionDescription => {
-                console.log('Sending offer', sessionDescription)            
-                rtcPeerConnection.setLocalDescription(sessionDescription)
-                socket.emit('offer', {
-                    type: 'offer',
-                    sdp: sessionDescription,
-                    room: roomName
-                })
-            
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
-})
+const handleJoined = withRoomCheck(() => {
+  navigator.mediaDevices.getUserMedia(streamConstraints)
+      .then(stream => {
+        localStream = stream;
+        localVideo.srcObject = stream;
+        sendMessage('ready', roomName)
+      })
+      .catch(err => {
+        console.log('An error occurred', err)
+      })
+});
 
-socket.on('offer',  event =>
-{
-    if(!isCaller)
-    {
-        rtcPeerConnection = new RTCPeerConnection(iceServers)
-        rtcPeerConnection.onicecandidate = onIceCandidate
-        rtcPeerConnection.ontrack = onAddStream
-        rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream)
-        rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream)
-        console.log('Recieved offer', event)            
-        rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event))
+const handleFull = withRoomCheck(() => {
+  alert('The following session is full, please create another session or wait')
+});
+
+const handleReady = withRoomCheck(() => {
+  if (!isCaller) {
+    console.warn('not a caller, nothing to do on \'ready\'');
+    return;
+  }
+  rtcPeerConnection = new RTCPeerConnection(rtcConfig);
+  rtcPeerConnection.onicecandidate = onIceCandidate;
+  rtcPeerConnection.ontrack = onAddStream;
+  rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream);
+  rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream);
+  rtcPeerConnection.createOffer()
+      .then(sessionDescription => {
+        console.log('Sending offer', sessionDescription);
+        rtcPeerConnection.setLocalDescription(sessionDescription)
+            .then(() => {
+              sendMessage('offer', {
+                sdp: sessionDescription,
+                room: roomName
+              });
+            });
+      })
+      .catch(err => {
+        console.log(err)
+      })
+});
+
+const handleOffer = withRoomCheck(({sdp}) => {
+  if (isCaller) {
+    console.warn('is caller, nothing to do on \'offer\'');
+    return;
+  }
+  rtcPeerConnection = new RTCPeerConnection(rtcConfig);
+  rtcPeerConnection.onicecandidate = onIceCandidate;
+  rtcPeerConnection.ontrack = onAddStream;
+  rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream);
+  rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream);
+  console.log('Received offer', sdp);
+  rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(sdp))
+      .then(() => {
         rtcPeerConnection.createAnswer()
             .then(sessionDescription => {
-                console.log('Sending answer', sessionDescription)            
-                rtcPeerConnection.setLocalDescription(sessionDescription)
-                socket.emit('answer', {
-                    type: 'answer',
-                    sdp: sessionDescription,
-                    room: roomName
-                })
+              console.log('Sending answer', sessionDescription);
+              rtcPeerConnection.setLocalDescription(sessionDescription)
+                  .then(() => {
+                    sendMessage('answer', {
+                      sdp: sessionDescription,
+                      room: roomName
+                    });
+                  });
             })
             .catch(err => {
-                console.log(err)
-            })
-    }
-})
+              console.log(err)
+            });
+      });
+});
 
-socket.on('answer', event => 
-{
-    console.log('Recieved answer', event)            
-    rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event))
-})
+const handleAnswer = withRoomCheck(({sdp}) => {
+  console.log('Received answer', sdp);
+  rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(sdp));
+});
 
-socket.on('candidate', event => 
-{
-    const candidate = new RTCIceCandidate(
-    {
-      sdpMLineIndex: event.label,
-      candidate: event.candidate  
+const handleCandidate = withRoomCheck(event => {
+  const candidate = new RTCIceCandidate(
+      {
+        sdpMLineIndex: event.label,
+        candidate: event.candidate
+      });
+  console.log('Received candidate', candidate);
+  rtcPeerConnection.addIceCandidate(candidate)
+});
+
+function onAddStream(event) {
+  remoteVideo.srcObject = event.streams[0];
+  remoteStream = event.streams[0]
+}
+
+function onIceCandidate(event) {
+  if (event.candidate) {
+    console.log('sending ice candidate', event.candidate);
+    sendMessage('candidate', {
+      label: event.candidate.sdpMLineIndex,
+      id: event.candidate.sdpMid,
+      candidate: event.candidate.candidate,
+      room: roomName
     })
-    console.log('Recieved candidate', candidate)
-    rtcPeerConnection.addIceCandidate(candidate)
-})
+  }
+}
 
-function onAddStream(event)
-{
-    remoteVideo.srcObject = event.streams[0]
-    remoteStream = event.streams[0]
-}
-function onIceCandidate(event)
-{
-    if(event.candidate)
-    {
-        console.log('sending ice candidate', event.candidate)
-        socket.emit('candidate', {
-            type: 'candidate',
-            label: event.candidate.sdpMLineIndex,
-            id: event.candidate.sdpMLineIndex,
-            candidate: event.candidate.candidate,
-            room: roomName
-        })
-    }
-}
+const handlersMap = {
+  created: handleCreated,
+  joined: handleJoined,
+  full: handleFull,
+  ready: handleReady,
+  offer: handleOffer,
+  answer: handleAnswer,
+  candidate: handleCandidate
+};
+
+ws.onmessage = (evt) => {
+  if (evt.data === 'ping') {
+    return;
+  }
+  const jsonMsg = JSON.parse(evt.data);
+  const handler = handlersMap[jsonMsg.command];
+  handler?.(jsonMsg.data.room, jsonMsg.data);
+};
